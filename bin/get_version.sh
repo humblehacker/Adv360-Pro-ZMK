8a4f914003d812450cb36fc9fecd7c2041841dba
David Whetstone <david@humblehacker.com>
2024-03-17 20:42:56 -0700

    improvements to get_version

--- bin/get_version.sh
+++ bin/get_version.sh
@@ -2,59 +2,51 @@
 
 # Get the date, first 4 chars of branch name and short commit hash
 date=$(date -u +"%Y%m%d")
-branch=${1:-$(git rev-parse --abbrev-ref HEAD | cut -c1-4)}
+branch=${1:-$(git rev-parse --abbrev-ref HEAD)}
 commit=${2:-$(git rev-parse --short HEAD)}
-
-uppercase_char() {
-    local char=$1
-
-    (echo $char | tr '[a-z]' '[A-Z]' 2> /dev/null) || echo "${char^^}"
-}
+dirty=$(git diff-index --quiet HEAD && echo "" || echo "dirty")
 
 # Function to transform characters to ZMK key behaviours
 transform_char() {
     local char=$1
-
-    if [[ $char =~ [A-Za-z] ]]; then
-        echo "<&kp $(uppercase_char $char)>, "
-    elif [[ $char =~ [0-9] ]]; then
-        echo "<&kp N${char}>, "
-    elif [ "$char" = "." ]; then
-        echo "<&kp DOT>, "
-    fi
+    case $char in
+        [A-Za-z]) echo "<&kp ${char^^}>" ;;
+        [0-9]) echo "<&kp N${char}>" ;;
+        ".") echo "<&kp DOT>" ;;
+        "-") echo "<&kp MINUS>" ;;
+        *) echo "Unknown character '"$char"'" > /dev/stderr; exit 1 ;;
+    esac
 }
 
-# Iterate over the date and format characters
-formatted_date=""
-for ((i = 0; i < ${#date}; i++)); do
-    formatted_date+=$(transform_char "${date:$i:1}")
-done
+# Function to transform a string into ZMK key behaviours
+transform_string() {
+    local str=$1
+    local -a transformed
+    for ((i = 0; i < ${#str}; i++)); do
+        transformed+=("$(transform_char "${str:$i:1}")") || exit 1
+    done
+    join_by ", " "${transformed[@]}"
+}
 
-# Insert separator between date and branch
-formatted_date+="<&kp MINUS>, "
+# Output a string with arguments joined by a separator (first argument)
+join_by() {
+    local sep=$1
+    shift
+    local first=$1
+    shift
+    printf "%s" "$first" "${@/#/$sep}"
+}
 
-# Iterate over the branch and format characters
-formatted_branch=""
-for ((i = 0; i < ${#branch}; i++)); do
-    formatted_branch+=$(transform_char "${branch:$i:1}")
-done
+# Combine the string parts, add trailing carriage return
+result=$(join_by "-" $date $branch $commit $dirty)
 
-# Insert separator between branch and commit hash
-formatted_branch+="<&kp MINUS>, "
+# Transform the combined string
+formatted_result=$(transform_string "$result")", <&kp RET>" || exit 1
 
-# Iterate over the commit hash and format characters
-formatted_commit=""
-for ((i = 0; i < ${#commit}; i++)); do
-    formatted_commit+=$(transform_char "${commit:$i:1}")
-done
-
-# Combine the formatted string, add trailing carriage return
-formatted_result="$formatted_date$formatted_branch$formatted_commit"
-formatted_result+="<&kp RET>"
-
+echo $result
 echo $formatted_result
-# Create new macro to define version, overwrite previous one
 
+# Create new macro to define version, overwrite previous one
 echo '#define VERSION_MACRO' > "config/version.dtsi"
 echo 'macro_ver: macro_ver {' >> "config/version.dtsi"
 echo 'compatible = "zmk,behavior-macro";' >> "config/version.dtsi"
